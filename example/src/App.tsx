74a8e6aab202a62d5bbd4d6c82facf12fd3557dc
M1NMAX <carlosaurora20@gmail.com>
2022-04-10 16:20:14 +0100

    added tailwind css

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'frontstate'
 import 'frontstate/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+import * as React from "react";
+import './tailwind.css'
 
-interface Props {
-  text: string
-}
-
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export function ExampleComponent() {
+  return <div>Example Component</div>;
 }
