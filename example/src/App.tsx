9d1cf6ed6338e6718275889b67e2720dc4863869
M1NMAX <carlosaurora20@gmail.com>
2022-04-12 14:01:14 +0100

    added textarea

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 
-import { Button, TextInput, Tooltip } from 'frontstate'
+import { Button, Textarea, TextInput, Tooltip } from 'frontstate'
 import 'frontstate/dist/index.css'
 
 const App = () => {
-  const [value, setValue] = useState('nf');
+  const [value, setValue] = useState('nf')
   console.log(value)
   return (
     <div className='flex flex-col items-center justify-center space-y-2 p-2 bg-blue-400'>
@@ -14,12 +14,25 @@ const App = () => {
       <Tooltip label='Tootiptext' position='right' compact>
         <button className='bg-red-500 w-12 h-8'>h</button>
       </Tooltip>
-      <TextInput label='heh' placeholder='heo' description='fhn' rounded='xl' required disabled />
+      <TextInput
+        label='heh'
+        placeholder='heo'
+        description='fhn'
+        rounded='xl'
+        required
+        disabled
+      />
       <TextInput
         label='heh'
         value={value}
         onChange={(e) => setValue(e.currentTarget.value)}
-        rounded='xl' size='xl'
+      />
+      <Textarea
+        label='heh'
+        value={value}
+        onChange={(e) => setValue(e.currentTarget.value)}
+        rounded='xl'
+        size='xl'
       />
     </div>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import './tailwind.css'
 
 export { Button } from './components/Button'
+export { Textarea } from './components/Textarea'
 export { TextInput } from './components/TextInput'
 export { Tooltip } from './components/Tooltip'
